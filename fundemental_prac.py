d374a52b5051b982bf0bfd301b980437fd96c0a3
aletterofthealphabet <wolfang393@gmail.com>
2021-06-16 09:40:04 -0700

    Added variables

--- fundemental_prac.py
+++ fundemental_prac.py
@@ -1,1 +1,23 @@
+# Practice printing, prints "Hello World"
 print("Hello World")
+
+# Practice with variables
+
+## STEP 1
+name = "Lebron"           ### Declared and initalized name
+age = 35                  ### Declared and initalized age
+salary = 37.44            ### Declared and initalized salary
+plays_basketball = True   ### Declared and initalized plays_basketball
+jersey_number = "23"      ### Declared and initalized jersey_number
+
+## STEP 2
+car_name = "Ford"
+x = 50
+my_first_name = "John"
+
+### Print the type of the variables in step 1
+print("name is " + type(name))
+print("age is " + type(age))
+print("salary is " + type(salary))
+print("plays_basketball is " + type(plays_basketball))
+print("jersey_number is " + type(jersey_number))
